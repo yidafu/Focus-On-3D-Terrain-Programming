e762f71f03e07a7fbde55d8078a3d1754bcaad77
yidafu <me@yidefu.dev>
2025-03-08 00:56:31 +0800

    feat(src): add kool CommonLauncher

--- src/commonMain/kotlin/dev/yidafu/terrain/CommonLauncher.kt
+++ src/commonMain/kotlin/dev/yidafu/terrain/CommonLauncher.kt
@@ -1,15 +1,25 @@
 package dev.yidafu.terrain
 
 import de.fabmax.kool.KoolContext
+import de.fabmax.kool.math.MutableVec3f
 import de.fabmax.kool.math.Vec3f
-import de.fabmax.kool.math.deg
 import de.fabmax.kool.modules.ksl.KslPbrShader
 import de.fabmax.kool.scene.addColorMesh
 import de.fabmax.kool.scene.defaultOrbitCamera
+import de.fabmax.kool.scene.geometry.PrimitiveType
 import de.fabmax.kool.scene.scene
 import de.fabmax.kool.util.Color
-import de.fabmax.kool.util.Time
 import de.fabmax.kool.util.debugOverlay
+import dev.yidafu.terrain.core.HeightMap
+
+private fun HeightMap.getVector3f(
+    x: Int,
+    y: Int,
+): Vec3f {
+    val z = get(x, y).toFloat() / 256f
+    println("Vec3f => x: $x, y: $y, z: $z")
+    return Vec3f(x.toFloat(), y.toFloat(), z * 16)
+}
 
 /**
  * Main application entry. This demo creates a small example scene, which you probably want to replace by your actual
@@ -17,36 +27,84 @@ import de.fabmax.kool.util.debugOverlay
  */
 fun launchApp(ctx: KoolContext) {
     // add a hello-world demo scene
-    ctx.scenes += scene {
-        // enable simple camera mouse control
-        defaultOrbitCamera()
-
-        // add a spinning color cube to the scene
-        addColorMesh {
-            generate {
-                // called once on init: generates a cube with different (vertex-)colors assigned to each side
-                cube {
-                    colored()
-                }
-            }
-            onUpdate {
-                // called on each frame: spins the cube around its x-axis by 45 degrees per second
-                transform.rotate(45f.deg * Time.deltaT, Vec3f.X_AXIS)
-            }
-            // assign a shader, which uses the vertex color info
-            shader = KslPbrShader {
-                color { vertexColor() }
-                metallic(0f)
-                roughness(0.25f)
-            }
+    val heightMap = MidpointDisplacement(1.5, 16).generate()
+    val stripList = mutableListOf<List<Vec3f>>()
+    for (y in 0..<heightMap.size - 1) {
+        val vectors = mutableListOf<Vec3f>()
+        for (x in 0..<(heightMap.size)) {
+            val v1 = heightMap.getVector3f(x, y)
+            val v2 = heightMap.getVector3f(x, y + 1)
+            vectors.add(v1)
+            vectors.add(v2)
         }
-
-        // set up a single light source
-        lighting.singleDirectionalLight {
-            setup(Vec3f(-1f, -1f, -1f))
-            setColor(Color.WHITE, 5f)
-        }
+        stripList.add(vectors)
     }
+
+    ctx.scenes +=
+        scene {
+            // enable simple camera mouse control
+            defaultOrbitCamera()
+            addColorMesh {
+                generate {
+                    cube {
+                        colored()
+                    }
+//                    grid {
+//                        sizeY = 32f
+//                        sizeX = 32f
+//                        color = Color.RED
+//                    }
+//                    grid {
+//                        sizeY = 32f
+//                        sizeX = 32f
+//                        color = Color.BLUE
+//                        xDir.set(Vec3f.Y_AXIS)
+//                    }
+
+//                    grid {
+//                        sizeY = 32f
+//                        sizeX = 32f
+//                        color = Color.GREEN
+//                        texCoordOffset.set(32f, 16f)
+//                        xDir.set(Vec3f.NEG_X_AXIS)
+//                        yDir.set(Vec3f.NEG_Y_AXIS)
+//                    }
+                }
+                shader =
+                    KslPbrShader {
+                        color { vertexColor() }
+                        metallic(0f)
+                        roughness(0.25f)
+                    }
+            }
+            stripList.forEach {
+                addColorMesh(
+                    name = "colorMesh2",
+                    primitiveType = PrimitiveType.TRIANGLE_STRIP,
+                ) {
+                    generate {
+                        triangularSurface(
+                            TriangularSurfaceProps().apply {
+                                triangles.addAll(it)
+                            },
+                        )
+                    }
+                    shader =
+                        KslPbrShader {
+                            color { vertexColor() }
+                            metallic(0f)
+                            roughness(0.25f)
+                        }
+                }
+
+            }
+
+            // set up a single light source
+            lighting.singleDirectionalLight {
+                setup(Vec3f(-1f, -1f, -1f))
+                setColor(Color.WHITE, 5f)
+            }
+        }
 
     // add the debugOverlay. provides an fps counter and some additional debug info
     ctx.scenes += debugOverlay()
--- src/commonMain/kotlin/dev/yidafu/terrain/TriangularSurface.kt
+++ src/commonMain/kotlin/dev/yidafu/terrain/TriangularSurface.kt
@@ -1,2 +1,22 @@
 package dev.yidafu.terrain
 
+import de.fabmax.kool.math.Vec3f
+import de.fabmax.kool.scene.geometry.MeshBuilder
+import de.fabmax.kool.util.Color
+
+class TriangularSurfaceProps {
+    var triangles = mutableListOf<Vec3f>()
+}
+
+fun MeshBuilder.triangularSurface(props: TriangularSurfaceProps) {
+    check(props.triangles.size > 3) {
+        "triangular surface vertex count must >= 4"
+    }
+    val indices =
+        props.triangles.map {
+            vertex(it, Vec3f.Z_AXIS).apply {
+                color = Color(it.z, it.z, it.z)
+            }
+        }
+    geometry.addIndices(indices)
+}
